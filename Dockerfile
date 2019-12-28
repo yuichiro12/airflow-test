7a9d34f19bfd54377ea5bbdb5cc6205330228498
yuichiro12 <talcnrhyme@gmail.com>
2019-12-28 20:40:39 +0900

    trigger ecs task

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.8.0-buster
+FROM python:3.7.4-buster
 
-ENV AIRFLOW_HOME /root/ airflow
+ENV AIRFLOW_HOME /root/airflow
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
 WORKDIR $AIRFLOW_HOME
-RUN apt-get update && apt-get install -y supervisor
+RUN apt-get update && apt-get install -y default-libmysqlclient-dev supervisor
 COPY ./requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY config/airflow.cfg $AIRFLOW_HOME/airflow.cfg
 COPY config/supervisord.conf /etc/supervisord.conf
-RUN airflow initdb
 CMD supervisord -c /etc/supervisord.conf
---
+++ dags/collector.py
@@ -0,0 +1,69 @@
+import os
+import boto3
+from airflow import DAG
+from airflow.contrib.operators.ecs_operator import ECSOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 12, 24),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+client = boto3.client(
+    service_name="ecs",
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    region_name="us-west-1",
+)
+
+dag = DAG('collector', default_args=default_args, schedule_interval=timedelta(days=1))
+
+task_definition = "taskdef-airflow-test-dev-aece9bcd"
+cluster = "cluster-airflow-test-dev"
+overrides = {
+    'containerOverrides': [{
+        'name': 'alpine',
+        'command': ['sleep', '60'],
+    }]
+}
+
+t1 = ECSOperator(
+    task_id="run_collector",
+    dag=dag,
+    task_definition=task_definition,
+    cluster=cluster,
+    overrides=overrides,
+    region_name="us-west-1",
+)
+# t2 = ECSOperator(
+#     task_id='sleep',
+#     bash_command='sleep 5',
+#     retries=3,
+#     dag=dag)
+#
+# templated_command = """
+#     {% for i in range(5) %}
+#         echo "{{ ds }}"
+#         echo "{{ macros.ds_add(ds, 7)}}"
+#         echo "{{ params.my_param }}"
+#     {% endfor %}
+# """
+#
+# t3 = ECSOperator(
+#     task_id='templated',
+#     bash_command=templated_command,
+#     params={'my_param': 'Parameter I passed in'},
+#     dag=dag)
+#
+# t2.set_upstream(t1)
+# t3.set_upstream(t1)
